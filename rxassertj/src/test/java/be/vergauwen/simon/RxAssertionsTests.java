7fcde81f181bebc9eb28cc79cbedd81606b4c2b3
Filip Maelbrancke <filip@maelbrancke.net>
2016-11-20 20:53:20 +0100

    Condition support for the rxjava1 module

--- rxassertj/src/test/java/be/vergauwen/simon/RxAssertionsTests.java
+++ rxassertj/src/test/java/be/vergauwen/simon/RxAssertionsTests.java
@@ -1,10 +1,17 @@
 package be.vergauwen.simon;
 
+import org.assertj.core.api.Condition;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import rx.Observable;
 import rx.observers.TestSubscriber;
+
+import java.util.Objects;
+import java.util.Set;
+
+import static org.assertj.core.condition.AllOf.allOf;
+import static org.assertj.core.util.Sets.newLinkedHashSet;
 
 public class RxAssertionsTests {
 
@@ -35,4 +42,219 @@ public class RxAssertionsTests {
                 .completes()
                 .withoutErrors();
     }
+
+    @Test
+    public void allItemsShouldMeetCondition() throws Exception {
+        Observable<String> observable = getJediStringEmittingObservable();
+
+        Condition<String> isNotNullOrEmpty = new Condition<String>() {
+            @Override
+            public boolean matches(String value) {
+                return value != null && !value.isEmpty();
+            }
+        };
+
+        RxAssertions.assertThatSubscriberTo(observable)
+                .completes()
+                .withoutErrors()
+                .eachItemMatches(isNotNullOrEmpty);
+    }
+
+    @Test(expected = AssertionError.class)
+    public void allItemsConditionMatcherShouldFail() throws Exception {
+        Observable<Integer> observable = Observable.just(1);
+
+        RxAssertions.assertThatSubscriberTo(observable)
+                .eachItemMatches(FAILING_CONDITION);
+    }
+
+    @Test
+    public void allItemsShouldMeetCombinedConditions() throws Exception {
+        Observable<String> observable = getJediStringEmittingObservable();
+
+        Condition<String> isNotNullOrEmpty = new Condition<String>() {
+            @Override
+            public boolean matches(String value) {
+                return value != null && !value.isEmpty();
+            }
+        };
+        Condition<String> isJedi = new Condition<String>("jedi") {
+            @Override
+            public boolean matches(String value) {
+                return JEDIS.contains(value);
+            }
+        };
+
+        RxAssertions.assertThatSubscriberTo(observable)
+                .completes()
+                .withoutErrors()
+                .eachItemMatches(allOf(isNotNullOrEmpty, isJedi));
+    }
+
+    @Test
+    public void allItemsShouldNotMeetCondition() throws Exception {
+        Observable<String> observable = getJediStringEmittingObservable();
+
+        Condition<String> isNull = new Condition<String>() {
+            @Override
+            public boolean matches(String value) {
+                return value == null;
+            }
+        };
+
+        RxAssertions.assertThatSubscriberTo(observable)
+                .completes()
+                .withoutErrors()
+                .noItemNotMatches(isNull);
+    }
+
+    @Test
+    public void atLeastOneItemShouldMeetCondition() throws Exception {
+        Observable<String> observable = getJediStringEmittingObservable();
+
+        Condition<String> isLuke = new Condition<String>() {
+            @Override
+            public boolean matches(String value) {
+                return value.equals("Luke");
+            }
+        };
+
+        RxAssertions.assertThatSubscriberTo(observable)
+                .completes()
+                .withoutErrors()
+                .atLeastOneItemMatches(isLuke);
+    }
+
+    @Test(expected = AssertionError.class)
+    public void atLeastOneConditionCheckShouldFail() throws Exception {
+        Observable<String> observable = getJediStringEmittingObservable();
+
+        Condition<String> isDarthVader = new Condition<String>() {
+            @Override
+            public boolean matches(String value) {
+                return value.equals("Darth Vader");
+            }
+        };
+
+        RxAssertions.assertThatSubscriberTo(observable)
+                .completes()
+                .withoutErrors()
+                .atLeastOneItemMatches(isDarthVader);
+    }
+
+    @Test
+    public void atLeastTwoItemsShouldMeetCondition() throws Exception {
+        Observable<String> observable = getJediStringEmittingObservable();
+
+        Condition<String> containsTheLetterA = new Condition<String>() {
+            @Override
+            public boolean matches(String value) {
+                return value.contains("a");
+            }
+        };
+
+        RxAssertions.assertThatSubscriberTo(observable)
+                .completes()
+                .withoutErrors()
+                .areAtLeast(2, containsTheLetterA);
+    }
+
+    @Test(expected = AssertionError.class)
+    public void atLeastConditionCheckShouldFail() throws Exception {
+        Observable<String> observable = getJediStringEmittingObservable();
+
+        Condition<String> containsTheLetterA = new Condition<String>() {
+            @Override
+            public boolean matches(String value) {
+                return value.contains("a");
+            }
+        };
+
+        RxAssertions.assertThatSubscriberTo(observable)
+                .completes()
+                .withoutErrors()
+                .areAtLeast(3, containsTheLetterA);
+    }
+
+    @Test
+    public void atMostTwoItemsShouldMeetCondition() throws Exception {
+        Observable<String> observable = getJediStringEmittingObservable();
+
+        Condition<String> containsTheLetterA = new Condition<String>() {
+            @Override
+            public boolean matches(String value) {
+                return value.contains("a");
+            }
+        };
+
+        RxAssertions.assertThatSubscriberTo(observable)
+                .completes()
+                .withoutErrors()
+                .areAtMost(2, containsTheLetterA);
+    }
+
+    @Test(expected = AssertionError.class)
+    public void atMostConditionCheckShouldFail() throws Exception {
+        Observable<String> observable = getJediStringEmittingObservable();
+
+        Condition<String> containsTheLetterA = new Condition<String>() {
+            @Override
+            public boolean matches(String value) {
+                return value.contains("a");
+            }
+        };
+
+        RxAssertions.assertThatSubscriberTo(observable)
+                .completes()
+                .withoutErrors()
+                .areAtMost(1, containsTheLetterA);
+    }
+
+    @Test
+    public void exactlyTwoItemsShouldMeetCondition() throws Exception {
+        Observable<String> observable = getJediStringEmittingObservable();
+
+        Condition<String> containsTheLetterA = new Condition<String>() {
+            @Override
+            public boolean matches(String value) {
+                return value.contains("a");
+            }
+        };
+
+        RxAssertions.assertThatSubscriberTo(observable)
+                .completes()
+                .withoutErrors()
+                .areExactly(2, containsTheLetterA);
+    }
+
+    @Test(expected = AssertionError.class)
+    public void areExactlyConditionCheckShouldFail() throws Exception {
+        Observable<String> observable = getJediStringEmittingObservable();
+
+        Condition<String> containsTheLetterA = new Condition<String>() {
+            @Override
+            public boolean matches(String value) {
+                return value.contains("a");
+            }
+        };
+
+        RxAssertions.assertThatSubscriberTo(observable)
+                .completes()
+                .withoutErrors()
+                .areExactly(3, containsTheLetterA);
+    }
+
+    private static Set<String> JEDIS = newLinkedHashSet("Luke", "Yoda", "Obiwan");
+
+    private Observable<String> getJediStringEmittingObservable() {
+        return Observable.from(JEDIS);
+    }
+
+    private static Condition<Object> FAILING_CONDITION = new Condition<Object>() {
+        @Override
+        public boolean matches(Object value) {
+            return false;
+        }
+    };
+
 }
