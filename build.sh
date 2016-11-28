70d6d8285029e7d3915a7b160f1f03f5f45f31e6
Simon Vergauwen <vergauwen.simon@gmail.com>
2016-11-28 22:35:49 +0100

    update buildscript

--- build.sh
+++ build.sh
@@ -6,8 +6,14 @@ PROJECT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 "$PROJECT_DIR"/gradlew --no-daemon --info clean build codeCoverageReport
 
-if [ "$TRAVIS_BRANCH" != "$BRANCH" ]; then
+if [ "$TRAVIS_JDK_VERSION" != "$JDK" ]; then
+  echo "Skipping snapshot deployment: wrong JDK. Expected '$JDK' but was '$TRAVIS_JDK_VERSION'."
+elif [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+  echo "Skipping snapshot deployment: was pull request."
+elif [ "$TRAVIS_BRANCH" != "$BRANCH" ]; then
+  echo "Skipping snapshot deployment: wrong branch. Expected '$BRANCH' but was '$TRAVIS_BRANCH'."
+else
   echo "Deploying snapshot..."
-  "$PROJECT_DIR"/gradlew --no-daemon --info bintrayUpload
+  "$PROJECT_DIR"/gradlew --no-daemon --info rx2assertj:bintrayUpload
   echo "Snapshot deployed!"
 fi
